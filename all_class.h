6cf7d94c0e2721f44e01364c38cef3a837647442
Sanjay <sssv96@gmail.com>
2015-09-16 17:22:18 +0530

    1) Added initial class definitions. 2) Added initial function definitions for certain classes.

--- all_class.h
+++ all_class.h
@@ -8,17 +8,48 @@ class HVAL
 	{
 		this->val = val;
 	}
-	bool operator<(HVAL other_hval)
-	{
-		return hval < other_hval.val;
-	}
+	bool operator<(HVAL hval);
+	void update( unsigned char xold,unsigned char yold,unsigned char xnew,unsigned char ynew );
 };
 
-class STATE
+struct MOVE
 {
-	unsigned char pos_of[2][8];
-	HVAL my_hval,parent_hval;
-	public:
-	void gen_all_chidren();
-	void eval_config;
+	unsigned char xold : 4;
+	unsigned char yold : 4;
+	unsigned char xnew : 4;
+	unsigned char ynew : 4;
+	HVAL hval;
+	bool operator<(MOVE move);
 };
+
+struct POS_elem
+{
+	unsigned char x : 4;
+	unsigned char y : 4;
+};
+
+struct BOARD_elem
+{
+	unsigned char player : 2;// 0,1 or 2
+};
+
+class GAME
+{
+	static BOARD_elem board[8][8];
+	static POS_elem	  pos_of[2][8];
+	static int const MAX_LVL = 10;
+	static int max_lvl, curr_lvl=0;
+
+	list<move> moves;
+
+public:
+
+	GAME(int board[2][8], int max_turns); /*Game starts here*/ 
+	GAME(MOVE move);					  /*Spawn a new game where the configuration differs by 'move'*/
+	GAME(MOVE move, int parnt_hval );	  /*Spawn a new game where the configuration differs by 'move',
+											knowing that the parent has minimax value of 'parent_hval'*/
+
+	void find_moves();
+	void find_hval_of_moves();
+	void sort_moves();
+}
--- all_func.h
+++ all_func.h
@@ -1,3 +1,22 @@
+#include"all_class.h"
+
 /* functions operating on objects */
 
+bool HVAL::operator<(HVAL other_hval)
+{
+	return hval < other_hval.val;
+}
 
+void HVAL::update( unsigned char xold,unsigned char yold,unsigned char xnew,unsigned char ynew )
+/*
+ * Instead of computing the heuristic each time, we just calculate the value by looking at the move made
+ */
+{
+	if( !(xold<=xnew && yold<=ynew) )
+	{
+		cerr << '(' << xold << ',' << ynew << ")->(" << xnew << ',' << ynew ") is not possible.\n";
+		exit(11);
+	}
+
+	val += ( ynew-yold ); /* THIS IS delta(HEURISTIC) */
+}
